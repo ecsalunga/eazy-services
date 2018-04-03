e9805a6dfbb00066ad3c8bca3872b7aa3ca0d653
Emmanuel Salunga <emmanuel.salunga@gmail.com>
2018-04-03 14:04:02 +0800

    add: duplex data access

--- src/app/core/data/index.ts
+++ src/app/core/data/index.ts
@@ -3,3 +3,4 @@ export { DataLayer } from './layer';
 export { DataItem } from './item';
 export { DataItems } from './items';
 export { DataSetting } from './setting';
+export { Duplex } from './duplex';
