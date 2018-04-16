06b39e1d96bb3b65651ad0388b05e3ef582f6f73
Emmanuel Salunga <emmanuel.salunga@gmail.com>
2018-04-16 14:53:00 +0800

    add: public food types

--- src/app/food/type/food-type.component.ts
+++ src/app/food/type/food-type.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EmmLibCoreService, MenuItem } from '../../core';
+
 @Component({
   selector: 'food-type',
   templateUrl: './food-type.component.html',
   styleUrls: ['./food-type.component.css']
 })
 export class FoodTypeComponent implements OnInit {
+  Items: Array<MenuItem>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public core: EmmLibCoreService) {
+    this.core.DL.State.Title = "Food Types";
+    this.Items = new Array<MenuItem>();
   }
 
+  Process(item: MenuItem) {
+    console.log(item);
+  }
+
+  ngOnInit() {
+    this.core.DL.FoodTypes.forEach(type => {
+        let item = new MenuItem();
+        item.Selector = type.key;
+        item.ImgSrc = type.ImageUrl;
+        item.Title = type.Name;
+        item.Blurb = `( ${type.Category} ) ` + type.Blurb;
+
+        this.Items.push(item);
+    });
+  }
 }
