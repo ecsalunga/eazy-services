93c3b207dea2c2ae25586d18ff94a5df6494d567
Emmanuel Salunga <emmanuel.salunga@gmail.com>
2018-04-16 15:33:53 +0800

    add: public food sources

--- src/app/food/source/food-source.component.ts
+++ src/app/food/source/food-source.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { EmmLibCoreService  } from '../../core';
+import { EmmLibCoreService, MenuItem } from '../../core';
 
 @Component({
   selector: 'food-source',
@@ -8,13 +8,27 @@ import { EmmLibCoreService  } from '../../core';
   styleUrls: ['./food-source.component.css']
 })
 export class FoodSourceComponent implements OnInit {
+  Items: Array<MenuItem>;
 
   constructor(private core: EmmLibCoreService) { 
-    this.core.DL.State.Title = "Food Source";
+    this.core.DL.State.Title = "Food Sources";
+    this.Items = new Array<MenuItem>();
+  }
+
+  Process(item: MenuItem) {
+    console.log(item);
   }
 
   ngOnInit() {
-    
+    this.core.DL.FoodSources.forEach(source => {
+      let item = new MenuItem();
+      item.Selector = source.key;
+      item.ImgSrc = source.ImageUrl;
+      item.Title = source.Name;
+      item.Blurb = source.Blurb;
+
+      this.Items.push(item);
+  });
   }
 
 }
